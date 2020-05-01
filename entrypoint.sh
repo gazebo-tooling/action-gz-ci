b8ea7e926af62f2960d26bcf8c51765326ebd19a
Louise Poubel <louise@openrobotics.org>
2020-04-30 22:16:31 -0700

    coverage optional
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,11 @@
 
 set -x
 
+APT_DEPENDENCIES=$1
+COVERAGE=$2
+
 cd $GITHUB_WORKSPACE
 
-echo "Install base dependencies"
 apt update
 apt -y install wget lsb-release gnupg
 sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" > /etc/apt/sources.list.d/gazebo-stable.list'
@@ -23,29 +25,26 @@ update-alternatives \
   --slave /usr/bin/g++ g++ /usr/bin/g++-8 \
   --slave /usr/bin/gcov gcov /usr/bin/gcov-8
 
-echo "Install lcov"
 git clone https://github.com/linux-test-project/lcov.git -b v1.14
 cd lcov
 make install
 cd ..
 
-echo "Static checking before building - fail fast"
 sh tools/code_check.sh
 
-echo "Install package dependencies"
-apt -y install $1
+apt -y install $APT_DEPENDENCIES
 
-echo "Compile package"
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=coverage
 make
 
-echo "Test package"
 export CTEST_OUTPUT_ON_FAILURE=1
 make test
-make coverage VERBOSE=1
 
-echo "Codecov results"
-# disable gcov output with `-X gcovout -X gcov`
-run: bash <(curl -s https://codecov.io/bash) -X gcovout -X gcov
+if [ "$COVERAGE" = true ] ; then
+  make coverage VERBOSE=1
+
+  # disable gcov output with `-X gcovout -X gcov`
+  run: bash <(curl -s https://codecov.io/bash) -X gcovout -X gcov
+fi
