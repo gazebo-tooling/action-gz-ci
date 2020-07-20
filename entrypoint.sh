5d96ef6437a6db258ac42e8df312716f7ef86aa3
Louise Poubel <louise@openrobotics.org>
2020-07-20 10:44:57 -0700

    Collapse groups
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,6 +14,7 @@ SCRIPT_AFTER_MAKE_TEST="../.github/ci-bionic/after_make_test.sh"
 
 cd $GITHUB_WORKSPACE
 
+echo ::group::Dependencies from binaries
 apt update
 apt -y install wget lsb-release gnupg
 sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" > /etc/apt/sources.list.d/gazebo-stable.list'
@@ -30,19 +31,27 @@ apt -y install \
   $APT_DEPENDENCIES
 
 pip3 install -U pip vcstool colcon-common-extensions
+echo ::endgroup::
 
+echo ::group::GCC 8
 update-alternatives \
   --install /usr/bin/gcc gcc /usr/bin/gcc-8 800 \
   --slave /usr/bin/g++ g++ /usr/bin/g++-8 \
   --slave /usr/bin/gcov gcov /usr/bin/gcov-8
+echo ::endgroup::
 
-git clone https://github.com/linux-test-project/lcov.git -b v1.14
+echo ::group::Install LCOV
+git clone https://github.com/linux-test-project/lcov.git -b v1.14 2>&1
 cd lcov
 make install
 cd ..
+echo ::endgroup::
 
-sh tools/code_check.sh
+echo ::group::Code check
+sh tools/code_check.sh 2>&1
+echo ::endgroup::
 
+echo ::group::Dependencies from source
 if [ -f ".github/ci-bionic/dependencies.yaml" ] ; then
   mkdir -p deps/src
   cd deps
@@ -54,34 +63,50 @@ fi
 
 mkdir build
 cd build
+echo ::endgroup::
 
+echo ::group::Script before cmake
 if [ ! -z "$SCRIPT_BEFORE_CMAKE" ] ; then
   . $SCRIPT_BEFORE_CMAKE
 fi
+echo ::endgroup::
 
+echo ::group::cmake
 if [ ! -z "$CODECOV_TOKEN" ] ; then
   cmake .. $CMAKE_ARGS -DCMAKE_BUILD_TYPE=coverage
 else
   cmake .. $CMAKE_ARGS
 fi
+echo ::endgroup::
 
+echo ::group::Script between cmake and make
 if [ ! -z "$SCRIPT_BETWEEN_CMAKE_MAKE" ] ; then
-  . $SCRIPT_BETWEEN_CMAKE_MAKE
+  . $SCRIPT_BETWEEN_CMAKE_MAKE 2>&1
 fi
+echo ::endgroup::
 
+echo ::group::make
 make
+echo ::endgroup::
 
+echo ::group::Script after make
 if [ ! -z "$SCRIPT_AFTER_MAKE" ] ; then
-  . $SCRIPT_AFTER_MAKE
+  . $SCRIPT_AFTER_MAKE 2>&1
 fi
+echo ::endgroup::
 
+echo ::group::make test
 export CTEST_OUTPUT_ON_FAILURE=1
 make test
+echo ::endgroup::
 
+echo ::group::Script after make test
 if [ ! -z "$SCRIPT_AFTER_MAKE_TEST" ] ; then
-  . $SCRIPT_AFTER_MAKE_TEST
+  . $SCRIPT_AFTER_MAKE_TEST 2>&1
 fi
+echo ::endgroup::
 
+echo ::group::codecov
 if [ ! -z "$CODECOV_TOKEN" ] ; then
   make coverage VERBOSE=1
 
@@ -90,3 +115,4 @@ if [ ! -z "$CODECOV_TOKEN" ] ; then
   # disable gcov output with `-X gcovout -X gcov`
   bash codecov.sh -t $CODECOV_TOKEN -X gcovout -X gcov
 fi
+echo ::endgroup::
